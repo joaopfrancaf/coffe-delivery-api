220472356c9ff41afcb97c7956e60bf836e4065a
João Pedro FF <joaofrancaf@hotmail.com>
2023-05-15 23:21:21 -0300

    fixed a typo in type "tipo"

--- src/main/java/com/example/coffe/delivery/java/Dto/CoffeDto/CoffeResponseDto.java
+++ src/main/java/com/example/coffe/delivery/java/Dto/CoffeDto/CoffeResponseDto.java
@@ -7,6 +7,6 @@ import java.util.List;
 public record CoffeResponseDto(String id,String name, String description, List<String> type, double price, String img) {
 
     public CoffeResponseDto(Coffe coffe) {
-        this(coffe.getId(), coffe.getName(), coffe.getDescription(), coffe.getTipo(), coffe.getPrice(), coffe.getImg());
+        this(coffe.getId(), coffe.getName(), coffe.getDescription(), coffe.getType(), coffe.getPrice(), coffe.getImg());
     }
 }
--- src/main/java/com/example/coffe/delivery/java/Entity/Coffe.java
+++ src/main/java/com/example/coffe/delivery/java/Entity/Coffe.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -20,14 +19,14 @@ public class Coffe {
     @Indexed
     private String id;
     private String name;
-    private List<String> tipo;
+    private List<String> type;
     private String description;
     private Double price;
     private String img;
 
     public Coffe (CoffeRequestDto dto) {
         this.name = dto.name();
-        this.tipo = dto.type();
+        this.type = dto.type();
         this.description = dto.description();
         this.price = dto.price();
         this.img = dto.img();
